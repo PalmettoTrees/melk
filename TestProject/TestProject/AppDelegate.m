5af73008c2a45ef3f7b403aaaf03d3af95fd4322
PalmettoTrees <thepalmettotree@gmail.com>
2015-03-23 02:13:40 -0400

    Initial commit.

---
+++ TestProject/TestProject/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  TestProject
+//
+//  Created by Palmetto Trees on 3/23/15.
+//  Copyright (c) 2015 None. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
